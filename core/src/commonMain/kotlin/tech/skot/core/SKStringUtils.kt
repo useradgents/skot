d528466be6637766809dc44990a58b14f7c89950
sgueniot-ua <s.gueniot@useradgents.com>
2025-10-23 21:00:28 +0200

    CHORE - Deprecate and move skFormat to commonMain
    This commit deprecates the `skFormat` extension function and moves its implementation to `commonMain`.
    
    - The `actual` implementations of `skFormat` in `androidMain` and `jvmMain` have been removed.
    - The `expect` function in `commonMain` has been replaced with a concrete implementation `this.format(*values)`.
    - `skFormat` is now deprecated, recommending the use of `String.format(Locale.getDefault(), *values)` instead.

--- core/src/commonMain/kotlin/tech/skot/core/SKStringUtils.kt
+++ core/src/commonMain/kotlin/tech/skot/core/SKStringUtils.kt
@@ -1,3 +1,10 @@
 package tech.skot.core
 
-expect fun String.skFormat(vararg values: Any): String
+@Deprecated(
+    message = "Use format(Locale.getDefault(), *values) instead",
+    replaceWith = ReplaceWith(
+        "this.format(Locale.getDefault(), *values)",
+         "java.util.Locale"
+    )
+)
+fun String.skFormat(vararg values: Any): String = this.format(*values)
